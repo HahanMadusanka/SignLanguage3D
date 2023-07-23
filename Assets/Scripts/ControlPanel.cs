95eb44f083a9b50ea06a34e94c75d3f6ff218218
HahanMadusanka <hashan.madusanka@aiken.com.lk>
2023-07-23 15:25:50 +0530

    added language detection English and sinhala

--- Assets/Scripts/ControlPanel.cs
+++ Assets/Scripts/ControlPanel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Android;
+using TextSpeech;
 using TMPro;
 using System;
 
@@ -24,18 +25,14 @@ public class ControlPanel : MonoBehaviour
     private const string EnglishLanguageCode = "en-US";
     private const string SinhalaLanguageCode = "si-LK";
 
-    private AndroidJavaObject speechRecognizer;
-    // Start is called before the first frame update
-
 
     void Start()
     {
-        // Initialize Android speech recognizer
-        using (var activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-        using (var activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity"))
-        {
-            speechRecognizer = new AndroidJavaObject("unity.SpeechRecognition", activityContext);
-        } 
+        SpeechToText.Instance.onResultCallback = OnFinalSpeechResult;
+
+#if UNITY_ANDROID
+        CheckPermission();
+#endif
 
     }
 
@@ -46,53 +43,43 @@ public class ControlPanel : MonoBehaviour
     }
 
     public void english(){
+        processingMode = "english";
         sinhalaSingView.SetActive(false);
         Debug.Log("english");
-          debugLog.text = "english";
+
+        StartListening(EnglishLanguageCode);
      }
 
     public void sinhala() {
+        processingMode = "sinhala";
         sinhalaSingView.SetActive(true);
         Debug.Log("sinhala");
-         debugLog.text = "sinhala";
+
+        StartListening(SinhalaLanguageCode);
     }
 
 
-    // Call this method to start listening for speech
-    public void StartListening()
+    void CheckPermission()
     {
-                       
-        // Set the language to English
-        speechRecognizer.Call("setLanguage", EnglishLanguageCode);
-
-        // Start listening for speech
-        speechRecognizer.Call("startListening");
-
-    }
-
-    // Call this method to switch the language to Sinhala
-    public void SwitchToSinhala()
-    {
-        // Set the language to Sinhala
-        speechRecognizer.Call("setLanguage", SinhalaLanguageCode);
-    }
-
-    // Event handler for recognition results (called from Java)
-    public void OnRecognitionResult(string result)
-    {
-        // Update the output text with the recognized speech
-        //outputText.text = result;
-         Debug.LogError(result);
-         debugLog.text = result;
-    }
-
-    private void OnDestroy()
-    {
-        // Release resources when the GameObject is destroyed
-        if (speechRecognizer != null)
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
         {
-            speechRecognizer.Call("destroy");
+            Permission.RequestUserPermission(Permission.Microphone);
         }
+
     }
 
- }
+
+    #region Speach to Text
+    private void StartListening(string code)
+    {
+        SpeechToText.Instance.Setting(code);
+        SpeechToText.Instance.StartRecording();
+    }
+
+    void OnFinalSpeechResult(string result)
+    {
+        debugLog.text = result;
+    }
+    #endregion
+
+}
